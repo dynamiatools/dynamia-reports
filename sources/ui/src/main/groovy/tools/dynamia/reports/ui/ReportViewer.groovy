99b22276339cb6c9e7d2f9fea1363586d415b69e
Mario Serrano <mario@dynamiasoluciones.com>
2019-05-26 06:11:44 -0500

    licensed to lgpl v3

--- sources/ui/src/main/groovy/tools/dynamia/reports/ui/ReportViewer.groovy
+++ sources/ui/src/main/groovy/tools/dynamia/reports/ui/ReportViewer.groovy
@@ -192,6 +192,28 @@ class ReportViewer extends Div implements ActionEventBuilder {
 
                 if (filter.dataType == DataType.ENUM && filter.enumClassName != null) {
                     EnumFilterProvider provider = ReportsUtils.findEnumFilterProvider(filter.enumClassName);
+
+/*-
+ * #%L
+ * DynamiaReports - UI
+ * %%
+ * Copyright (C) 2018 - 2019 Dynamia Soluciones IT SAS
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
                     if (provider != null) {
                         field.fieldClass = Class.forName(filter.enumClassName)
                         field.propertyInfo = new PropertyInfo(field.name, field.fieldClass, Report, AccessMode.READ_WRITE)
