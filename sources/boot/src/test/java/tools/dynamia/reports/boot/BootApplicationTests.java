30843c248dc33220078707fca0d4da2e14d7802d
Mario Serrano Leones <mario@dynamiasoluciones.com>
2024-04-19 09:06:08 -0500

    boot project

---
+++ sources/boot/src/test/java/tools/dynamia/reports/boot/BootApplicationTests.java
@@ -0,0 +1,13 @@
+package tools.dynamia.reports.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
